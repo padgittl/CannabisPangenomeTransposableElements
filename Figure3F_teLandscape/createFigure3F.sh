94b20b7b85dd87b3246ef9629b39d5f13797baf9
Lillian Padgitt-Cobb <lilianpagitt-cobb@lilliankpc.lan>
2024-05-30 23:57:43 -0700

    update

--- Figure3F_teLandscape/createFigure3F.sh
+++ Figure3F_teLandscape/createFigure3F.sh
@@ -10,6 +10,27 @@ wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/te_landsc
 wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/te_landscape_analysis/SODLb_invs_chr7.bed
 wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/te_landscape_analysis/SODLb_trans_chr7.bed
 
+# methylation data is available for download for the following genomes (each file is ~1 GB):
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/AH3Ma.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/AH3Mb.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/FCS1a.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/FCS1b.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/H3S1a.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/H3S1b.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/KCDv1a.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/KCDv1b.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/NLv1a.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/NLv1b.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/SAN2a.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/SAN2b.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/SN1v3a.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/SN1v3b.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/SODLa.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/SODLb.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/WHWa.loreme.bed
+wget https://salk-tm-pub.s3.us-west-2.amazonaws.com/cannabis_pangenome/csat_methylation/WHWb.loreme.bed
+
+
 # process data for figure -- this script takes a few minutes to run
 python scripts/teAndMethylationDensities.py SODLb.primary_high_confidence_chr7.gff3 SODLb.unmasked.fasta.mod.EDTA.TEanno.chr7.gff3 SODLb.loreme.chr7.bed SODLb.softmasked.chr7.fasta data/other_files/csat_orientations.tsv SODLb_dups_chr7.bed SODLb_invs_chr7.bed SODLb_trans_chr7.bed SODLb chr7 1000000 &
 
