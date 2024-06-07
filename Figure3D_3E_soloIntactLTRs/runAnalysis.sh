29842884d16d985e441d5ace6ee2347d3f23bea4
Lillian Padgitt-Cobb <lilianpagitt-cobb@lilliankpc.local>
2024-06-06 17:07:26 -0700

    update

--- Figure3D_3E_soloIntactLTRs/runAnalysis.sh
+++ Figure3D_3E_soloIntactLTRs/runAnalysis.sh
@@ -9,14 +9,19 @@ tar -xvzf SODLb.primary_high_confidence.gff3.tar.gz
 tar -xvzf SODLb.unmasked.fasta.mod.EDTA.TEanno.gff3.tar.gz
 
 # run analysis to identify solo LTRs -- this script takes a while to run, so if testing in a hurry, can use chr7 only
-python scripts/identifySoloLTRs.py SODLb.unmasked.fasta.mod.EDTA.TEanno.gff3 SODLb.primary_high_confidence.gff3 10 SODLb cannabis 5000
-python scripts/identifySoloLTRs.py SODLb.unmasked.fasta.mod.EDTA.TEanno.chr7.gff3 SODLb.primary_high_confidence.chr7.gff3 10 SODLb cannabis 5000
+# python scripts/identifySoloLTRs.py SODLb.unmasked.fasta.mod.EDTA.TEanno.gff3 SODLb.primary_high_confidence.gff3 10 SODLb cannabis 5000
+python scripts/identifySoloLTRs.py data/SODLb.unmasked.fasta.mod.EDTA.TEanno.chr7.gff3 data/SODLb.primary_high_confidence.chr7.gff3 10 SODLb cannabis 5000
+python scripts/identifySoloLTRs.py data/AH3Ma.unmasked.fasta.mod.EDTA.TEanno.chr1.gff3 data/AH3Ma.primary_high_confidence.chr1.gff3 10 AH3Ma cannabis 5000
+python scripts/identifySoloLTRs.py data/AH3Mb.unmasked.fasta.mod.EDTA.TEanno.chr5.gff3 data/AH3Mb.primary_high_confidence.chr5.gff3 10 AH3Mb cannabis 5000
 
 # concatenate output files
 cat *soloLTRFlankingWindow5000_candidateSoloLTRData.tsv > combinedFiltered_window5000bp.tsv
 cat *unfilteredSoloLTRs.tsv > combinedUnfiltered.tsv
-ls data/SODLb.unmasked.fasta.mod.EDTA.TEanno.chr7.gff3 > gffFileList.txt
+# ls data/SODLb.unmasked.fasta.mod.EDTA.TEanno.chr7.gff3 > gffFileList.txt
+ls data/*TEanno*gff3 > gffFileList.txt
 
-# calculate solo:intact LTR ratio and generate boxplot
+# calculate solo:intact LTR ratio and generate boxplot per chromosome
 python scripts/calculateSoloIntactRatio.py combinedUnfiltered.tsv combinedFiltered_window5000bp.tsv gffFileList.txt 5000 cannabis
 
+# calculate solo:intact LTR ratio for full assembly
+python scripts/calculateGlobalSoloIntactRatio.py combinedUnfiltered.tsv combinedFiltered_window5000bp.tsv gffFileList.txt 5000 cannabis
